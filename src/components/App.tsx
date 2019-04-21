68eec7af9a618afee740d330a1db5594b3edc955
r.nishio <quick.xp@gmail.com>
2019-04-22 00:44:01 +0900

    dev

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,12 +7,21 @@ import 'styles/entry.scss'
 
 import { Test } from './modules'
 import GlobalNavigation from 'components/molecules/GlobalNavigation'
+import IconSettings from '@salesforce/design-system-react/components/icon-settings'
+import Button from '@salesforce/design-system-react/components/button'
 
 const App = () => (
   <main>
     <GlobalStyle />
+    <IconSettings iconPath="/assets/icons">
+      <Button
+        iconCategory="utility"
+        iconName="download"
+        iconPosition="left"
+        label="Neutral Icon"
+      />
+    </IconSettings>
     <React.Suspense fallback={<div>Loading...</div>}>
-      <GlobalNavigation />
       <Switch>
         <Route path="/" component={Test} />
       </Switch>
