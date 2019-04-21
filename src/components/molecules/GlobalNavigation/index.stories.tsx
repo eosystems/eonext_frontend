4517aa9fd880019095dd06e7ff74ca36be29d44b
Shinsuke Nishio <nishio@densan-labs.net>
2019-04-21 12:56:25 +0900

    fix typo

--- src/components/molecules/GlobalNavigation/index.stories.tsx
+++ src/components/molecules/GlobalNavigation/index.stories.tsx
@@ -2,6 +2,6 @@ import * as React from 'react'
 import { storiesOf } from '@storybook/react'
 import GlobalNavigation from './'
 
-storiesOf('molecules/GlobakNavigation', module).add('with text', () => (
+storiesOf('molecules/GlobalNavigation', module).add('with text', () => (
   <GlobalNavigation>Hoge</GlobalNavigation>
 ))
